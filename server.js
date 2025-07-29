7ec3f8a5a69908d5a4d41ccddb785a1550fb3036
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-29 23:28:52 +0300

    Made a few changes to the server.js file

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 // creating a server in Node js
 const server = http.createServer((req, res) =>{
-    res.write("Hello World, the first server I have created in Node js.");
-    res.end();
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end('<h2>Welcome to the HomePage</h2>')
+    }else if(req.url === '/about'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end('<h2>About Page</h2>')
+    }else{
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end('<h2>404 Page Not Found</h2>')
+    }
+    // console.log(req.url);
+    // console.log(req.method);
+    // res.setHeader('Content-type', 'text/html')
+    res.writeHead(200 , {'content-type': 'text/html'});
+    // res.write("<h2>Hello World, the first server I have created in Node js.</h2>");
+    res.end("<h2>Hello World, the first server I have created in Node js.</h2>");
 })
 
 server.listen(PORT, () =>{
