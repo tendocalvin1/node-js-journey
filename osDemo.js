de67e805106ded8c289ea9ee3675568e120c133e
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-01 18:59:39 +0300

    Learning diffrerent methods in the OS module in Node JS

---
+++ osDemo.js
@@ -0,0 +1,16 @@
+// OS module // This module provides information about the system
+import os from 'os'; 
+
+// Methods in the OS module
+// userInfo
+console.log(os.userInfo());
+
+// Methods that have to do with memory
+// 1. totalmem()
+console.log(os.totalmem());
+
+// freemem()
+console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
