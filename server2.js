d0a89daeff49e4849cbe533e8dd22554a9df7004
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-01 17:28:19 +0300

    Testing my APIs using ThunderClient

--- server2.js
+++ server2.js
@@ -15,18 +15,24 @@ const logger = (req, res, next) =>{
     next();
 }
 
+// JSON middleware
+const jsonMiddleware = (req, res, next) =>{
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}
 
-const server = createServer((req, res) =>{
-    if(req.url === '/api/users' && req.method === 'GET'){
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    }else if(req.url.match(/\/api\/users\/([0-9]+)/)&& req.method ==='GET'){
-        const id = req.url.split('/')[3];
-        const user = users.find((user) => user.id === parseInt(id));
-        res.setHeader('Content-Type', 'application/json');
-        if(user){
-        
+// Route handler for GET /api/users
+const getUserHandler = (req, res) =>{
+    res.write(JSON.stringify(users));
+    res.end();
+}
+
+// Route handler for GET /api/users/:id
+const getUserByIdHandler = (req, res) =>{
+    const id = req.url.split('/')[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    if(user){
+            
         res.write(JSON.stringify(user));
         res.end();
         }else{
@@ -35,13 +41,48 @@ const server = createServer((req, res) =>{
         res.write(JSON.stringify('User not found'));
         res.end();
         }
-        
-    }else{
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 404;
-        res.write(JSON.stringify('Route not found'));
+}
+
+// Route Handler for POST /api/users
+const createUserHandler = (req, res) => {
+    let body = '';
+    // listen for data
+    req.on('data', (chunk) =>{
+        body += chunk.toString();
+    });
+
+    req.on('end', () =>{
+        const newUser = JSON.parse(body); // Here, we're turning JSON into a JavaScript object
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser)); // Here, we're turning a JavaScript object into JSON 
         res.end();
-    }
+    });
+}
+
+// Not found Handler
+const NotFoundHandler = (req, res) =>{
+    res.statusCode = 404;
+    res.write(JSON.stringify('User not found'));
+    res.end();
+}
+
+
+const server = createServer((req, res) =>{
+    logger(req, res, () =>{
+        jsonMiddleware(req, res, () =>{
+            if(req.url ==='/api/users' && req.method === 'GET'){
+                getUserHandler(req, res);
+            }else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+                getUserByIdHandler(req, res);
+            }else if(req.url ==='/api/users' && req.method === 'POST'){
+                createUserHandler(req, res);
+            }else{
+                NotFoundHandler(req, res);
+            }
+        })
+    })
+    
 })
 
 server.listen(PORT, () =>{
