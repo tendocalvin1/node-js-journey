537a56ae39be2d8c06097c830e8874adadbaee63
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-01 18:36:42 +0300

    Learning the basics of using Path module in Node JS

---
+++ pathDemo.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import url from 'url';
+const filePath = './dir1/dir2/test.txt';
+
+
+// basename() - This will return the last portion of the path
+console.log(path.basename(filePath));
+
+// dirname() -
+console.log(path.dirname(filePath));
+
+// extension name()- 
+console.log(path.extname(filePath));
+
+// parse()
+console.log(path.parse(filePath));
+
+const __filename = url.fileURLToPath(import.meta.url);  // this gives us the actual file path
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+
