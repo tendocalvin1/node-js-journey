90f2ddf83e8f83c2b9cf970df76d490c200ea169
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-01 09:55:40 +0300

    Added some details about the fs modules, filepath, http methods and try catch function

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname)
 // creating a server in Node js
-const server = http.createServer((req, res) =>{
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.end('<h2>Welcome to the HomePage</h2>')
-    }else if(req.url === '/about'){
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.end('<h2>About Page</h2>')
-    }else{
-        res.writeHead(404, {'Content-type': 'text/html'});
-        res.end('<h2>404 Page Not Found</h2>')
+const server = http.createServer(async(req, res) =>{
+    try{
+        // Check if GET request
+        if(req.method === 'GET'){
+            let filepath;
+            if(req.url === '/'){
+               filepath = path.join(__dirname,'public', 'index.html') 
+            }else if(req.url === '/about'){
+                filepath = path.join(__dirname,'public', 'about.html')
+            }else{
+               throw new Error('404 Not Found')
+            }
+
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        }else{
+            throw new Error('Method not allowed');
+        }
+    }catch(error){
+        res.writeHead(500, {'Content-type': 'text/plain'});
+        res.end('Server Error')
     }
+    // Error 500 is a server error
     // console.log(req.url);
     // console.log(req.method);
     // res.setHeader('Content-type', 'text/html')
-    res.writeHead(200 , {'content-type': 'text/html'});
+    //res.writeHead(200 , {'content-type': 'text/html'});
     // res.write("<h2>Hello World, the first server I have created in Node js.</h2>");
-    res.end("<h2>Hello World, the first server I have created in Node js.</h2>");
+    //res.end("<h2>Hello World, the first server I have created in Node js.</h2>");
 })
 
 server.listen(PORT, () =>{
