9741c0ba1aab8f73d7fca2868e5904c0994fda4f
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-01 11:28:29 +0300

    Handling middleware in Node JS

--- server2.js
+++ server2.js
@@ -8,18 +8,34 @@ const users = [
     {id:4 , name: 'Matheus Cunha'},
 ]
 
+// logger middleware
+// middleware normally takes in request, response and next
+const logger = (req, res, next) =>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+
 const server = createServer((req, res) =>{
     if(req.url === '/api/users' && req.method === 'GET'){
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
     }else if(req.url.match(/\/api\/users\/([0-9]+)/)&& req.method ==='GET'){
-        const id = req.url.split('')[3];
-        // console.log(id);
-        const user = users.find((user) => user.id =parseInt(id));
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
         res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify({id: 1, name:'David Beckham'}));
+        if(user){
+        
+        res.write(JSON.stringify(user));
         res.end();
+        }else{
+       
+        res.statusCode = 404;
+        res.write(JSON.stringify('User not found'));
+        res.end();
+        }
+        
     }else{
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
